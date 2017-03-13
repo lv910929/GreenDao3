2a795a6ef76bb03744f3aeca8a4bdbba1d8efc1f
lv910929 <lv910929@live.com>
2017-03-13 15:05:49 +0800

    add layout_message_item.xml

--- app/src/main/java/com/lv/greendao3/model/Message.java
+++ app/src/main/java/com/lv/greendao3/model/Message.java
@@ -1,10 +1,14 @@
 package com.lv.greendao3.model;
 
+import org.greenrobot.greendao.annotation.Entity;
+
 import java.io.Serializable;
 
 /**
  * Created by Lv on 2017/3/7.
  */
-
+@Entity
 public class Message implements Serializable{
+
+
 }
